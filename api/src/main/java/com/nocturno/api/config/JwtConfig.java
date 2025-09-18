a96b3e1e33aacadbc90f83e78b41365aaf60813d
Luís Filipe <lufimelo@outlook.com>
2025-09-18 19:14:53 -0300

    refactor(server): reorganize cookei authentication

--- api/src/main/java/com/nocturno/api/config/JwtConfig.java
+++ api/src/main/java/com/nocturno/api/config/JwtConfig.java
@@ -2,14 +2,20 @@ package com.nocturno.api.config;
 
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationProvider;
 
 import com.nimbusds.jose.jwk.JWK;
 import com.nimbusds.jose.jwk.JWKSet;
@@ -36,5 +42,21 @@ public class JwtConfig {
     @Bean
     public JwtDecoder jwtDecoder(){
         return NimbusJwtDecoder.withPublicKey(this.rsaPublicKey).build();
+    }
+
+    @Bean
+    public JwtAuthenticationProvider jwtAuthenticationProvider(JwtDecoder jwtDecoder) {
+        JwtAuthenticationProvider provider = new JwtAuthenticationProvider(jwtDecoder);
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(jwt -> 
+            List.of(new SimpleGrantedAuthority("SCOPE_" + jwt.getClaimAsString("scope")))
+        );
+        provider.setJwtAuthenticationConverter(converter);
+        return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(JwtAuthenticationProvider provider) {
+        return new ProviderManager(provider);
     }
 }
