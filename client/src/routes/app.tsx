41e229b2e380f4eb3fd6cc2494a974ba8187c0d3
Luís Filipe <lufimelo@outlook.com>
2026-01-28 18:45:52 -0300

    feat(client): add authentication context and initial posts timeline

--- client/src/routes/app.tsx
+++ client/src/routes/app.tsx
@@ -1,14 +1,71 @@
 "use client";
 
+import { useEffect, useState } from "react";
+import { useAuth } from "@/context/auth-provider";
+
+type Post = {
+  id: number;
+  content: string;
+  createdAt: string;
+  creatorUsername: string;
+  numberLikes: number;
+};
+
+
 function App() {
+  const [posts, setPosts] = useState<Post[]>([]);
+  const { isAuth, setAuth } = useAuth();
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch("http://localhost:8080/api/post", {
+          credentials: "include",
+        });
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+
+        setPosts(data);
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+        setAuth(false)
+      }
+    };
+
+    fetchPosts();
+  });
+
   return (
-    <div className="grow-1 flex justify-center text-center flex-col max-w-2xl gap-2">
-      <h1 className="text-2xl font-semibold">Welcome to Nocturno</h1>
-      <p>
-        Nocturno é uma rede social para compartilhar seu dia a dia, conhecer
-        novas pessoas, criar laços genuínos e se conectar por meio de conversas.
-      </p>
-    </div>
+    <div className="grow w-xl flex text-center flex-col gap-4 my-2.5">
+    {isAuth &&
+      posts.map((post) => (
+        <div
+          key={post.id}
+          className="w-auto border rounded-lg p-4 text-left shadow-sm"
+        >
+          <p className="font-semibold">@{post.creatorUsername}</p>
+          <p className="my-2">{post.content}</p>
+
+          <div className="text-sm text-gray-500 flex justify-between">
+            <span>{new Date(post.createdAt).toLocaleString()}</span>
+            <span>❤️ {post.numberLikes}</span>
+          </div>
+        </div>
+    ))}
+    {!isAuth && (
+      <div className="my-auto">
+        <h1 className="text-2xl font-semibold">Welcome to Nocturno</h1>
+        <p>
+          Nocturno é uma rede social para compartilhar seu dia a dia, conhecer
+          novas pessoas, criar laços genuínos e se conectar por meio de conversas.
+        </p>
+      </div>
+    )}
+  </div>
   );
 }
 
--- client/src/routes/login.tsx
+++ client/src/routes/login.tsx
@@ -2,7 +2,7 @@ import LoginForm from "@/components/loginForm";
 
 export default function Login(){
     return (
-        <div className="grow-1 flex items-center py-4">
+        <div className="grow flex items-center py-4">
             <LoginForm />
         </div>
     )
